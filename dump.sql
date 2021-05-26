4afbb53fe388b5261b9b32aa0a2ad0bc0363b0ef
akolosov <a.kolosov@digital-lab.ru>
2021-05-26 23:25:30 +0500

    init

---
+++ dump.sql
@@ -0,0 +1,90 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.5 (Debian 10.5-1.pgdg90+1)
+-- Dumped by pg_dump version 10.5 (Debian 10.5-1.pgdg90+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: DATABASE postgres; Type: COMMENT; Schema: -; Owner: postgres
+--
+
+COMMENT ON DATABASE postgres IS 'default administrative connection database';
+
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: beer; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.beer (
+);
+
+
+ALTER TABLE public.beer OWNER TO postgres;
+
+--
+-- Name: person; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.person (
+    name character varying(188),
+    last_name character varying(188)
+);
+
+
+ALTER TABLE public.person OWNER TO postgres;
+
+--
+-- Data for Name: beer; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.beer  FROM stdin;
+\.
+
+
+--
+-- Data for Name: person; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.person (name, last_name) FROM stdin;
+pus	kus
+pus	\N
+pus	\N
+pus	\N
+pus	\N
+pus	\N
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
